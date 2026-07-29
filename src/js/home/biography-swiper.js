cb4befe01e12b141793c9b51530c9b317f151d10
3p-vln <vldknovak@gmail.com>
2026-07-29 17:06:28 +0300

    BIOGRAPHY (desktop & adapt)

---
+++ src/js/home/biography-swiper.js
@@ -0,0 +1,18 @@
+import {Swiper} from "swiper";
+
+export const biographySwiper = new Swiper('.info__date-swiper.swiper', {
+	slidesPerView: 'auto',
+	spaceBetween: 35,
+	
+	breakpoints: {
+		375: {
+			spaceBetween: 25,
+		},
+		1440: {
+			spaceBetween: 35,
+		},
+		1920: {
+			spaceBetween: 35,
+		}
+	}
+})
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import {burgerMenu} from "./components/burger-menu";
+import {biographySwiper} from "./home/biography-swiper";
 
 document.addEventListener('DOMContentLoaded', () => {
-	burgerMenu()
+	burgerMenu();
+	biographySwiper;
 })
